afa30f5cb81a50c0a745ae0135d6a50dcfa3d07b
Sayed hassan <88848951+SayedHsn69@users.noreply.github.com>
2022-03-29 21:24:24 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
+from pytube import*
+#يسأل عن رابط المقطع
+link = input("Enter the video link:  ")
+ytdata = YouTube(link)
+Path = r'C:\Users\person\Videos' #download path , edit this to ur folder path
+
+#يظهر التفاصيل
+print("Title: ",ytdata.title)
+print("Number of views: ",ytdata.views)
+print("Length of video: ",ytdata.length)
+print("Rating of video: ",ytdata.rating)
+#ياخذ المقطع بأعلى جودة ممكنة
+ys = ytdata.streams.get_highest_resolution()
+#يحمل
+print("Downloading...")
+ys.download(Path)
+print("Done!")
 
